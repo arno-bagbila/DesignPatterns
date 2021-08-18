1353d756132bf58e3011bdf2ff7eb464dbcc3f23
Arnaud Bagbila <arbagb@outlook.com>
2021-08-18 23:18:30 +0100

    builder exercice

---
+++ BuilderCodingExercice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderCodingExercice
+{
+
+
+   public class CodeBuilder
+   {
+      public string ClassName;
+      private List<string> elements = new();
+
+      public CodeBuilder(string className)
+      {
+         ClassName = className;
+      }
+
+      public CodeBuilder AddField(string name, string type)
+      {
+         elements.Add($"  public {type} {name};");
+         return this;
+      }
+
+      public override string ToString()
+      {
+         var sb = new StringBuilder();
+         sb.AppendLine($"public class {ClassName}");
+         sb.AppendLine();
+         sb.AppendLine("{");
+         foreach (var element in elements)
+         {
+            sb.AppendLine(element);
+         }
+
+         sb.AppendLine();
+         sb.Append("}");
+         return sb.ToString();
+      }
+   }
+
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+         Console.WriteLine(cb);
+
+
+      }
+   }
+}

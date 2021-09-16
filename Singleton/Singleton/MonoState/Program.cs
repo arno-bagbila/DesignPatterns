1b91239a90ae097677681f0488b2e13aa945f106
Arnaud Bagbila <arbagb@outlook.com>
2021-09-16 21:11:21 +0100

    Monostate pattern

---
+++ Singleton/Singleton/MonoState/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MonoState
+{
+   static class Program
+   {
+      // The singleton approach is to prevent people calling the constructor
+      public class CEO
+      {
+         private static string name;
+         private static int age;
+
+         public string Name
+         {
+            get => name;
+            set => name = value;
+         }
+
+         public int Age
+         {
+            get => age;
+            set => age = value;
+         }
+
+         public override string ToString()
+         {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+         }
+      }
+      static void Main(string[] args)
+      {
+         var ceo = new CEO();
+         ceo.Name = "Cedric";
+         ceo.Age = 30;
+
+         var ceo2 = new CEO();
+         ceo2.Age = 48;
+
+         Console.WriteLine(ceo);
+      }
+   }
+}
